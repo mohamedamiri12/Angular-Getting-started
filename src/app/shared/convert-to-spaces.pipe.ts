4a3c377f5d79669a6d8d8dbc55ac10c97bca50a5
mohamedamiri12 <amirimohamed1999@gmail.com>
2022-02-04 14:41:23 +0100

    initial commit

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,13 @@
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import  { Pipe, PipeTransform} from '@angular/core';
+
+@Pipe ({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform {
+    
+    transform(value: string, character: string): string {
+        return value.replace(character,' ');
+    }
+}
